e427694c6e1024450f023c1ebfd1b2ace76c48ab
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-11-05 13:38:09 +0200

    create migration for ratings table

---
+++ database/migrations/2019_11_05_113513_create_ratings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 10)->unique();
+            $table->integer('degree');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
